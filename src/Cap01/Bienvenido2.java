73247ecb70043a21f6281a0f2f4a37350f52d2f5
Hazael Lopez <elhazael134@gmail.com>
2020-04-11 10:44:16 -0600

    Modificamos nuestro primer ejercicio

--- src/Cap01/Bienvenido2.java
+++ src/Cap01/Bienvenido2.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Cap01;
+package Cap01;// Fig. 2.3: Bienvenido2.java
+// Imprimir una línea de texto con varias instrucciones.
 
-/**
- *
- * @author lhaza
- */
 public class Bienvenido2 {
-    
-}
+    // el método main empieza la ejecución de la aplicación en Java
+
+    public static void main(String args[]) {
+        System.out.print("Bienvenido a ");
+        System.out.println("la programacion en Java!");
+
+    } // fin del método main
+}// fin de la clase Bienvenido2
